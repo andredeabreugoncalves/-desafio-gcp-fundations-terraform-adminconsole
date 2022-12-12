c6448681af266a97d77ecbed0a11bb9c9546c3e1
André Goncalves <andre.sspsc@gmail.com>
2022-12-12 19:03:36 -0300

    commit add usuário google workspace

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 provider "googleworkspace" {
   credentials             = "${file("serviceaccount.yaml")}"
   customer_id             = "C04fayzre"
-  impersonated_estatisiticas_email = "andre@andreag.com.br"
+  impersonated_user_email = "andre@andreag.com.br"
   oauth_scopes = [
-    "https://www.googleapis.com/auth/admin.directory.estatisiticas",
-    "https://www.googleapis.com/auth/admin.directory.estatisiticasschema",
-    "https://www.googleapis.com/auth/admin.directory.group",	
+   "https://www.googleapis.com/auth/admin.directory.user",
+    "https://www.googleapis.com/auth/admin.directory.userschema",
+    "https://www.googleapis.com/auth/admin.directory.group",		
     # include scopes as needed
   ]
 }
 
-resource "googleworkspace_group" "estatisiticas" {
-  email       = "estatisiticas@andreag.com.br"
-  name        = "estatisiticas"
-  description = "Grupo estatisiticas"
+resource "googleworkspace_group" "estatisticas" {
+  email       = "estatisticas@andreag.com.br"
+  name        = "estatisticas"
+  description = "Grupo estatisticas"
 
 
   timeouts {
@@ -22,20 +22,20 @@ resource "googleworkspace_group" "estatisiticas" {
   }
 }
 
-resource "googleworkspace_estatisiticas" "estatisiticas" {
-  primary_email = "estatisiticas@andreag.com.br"
+resource "googleworkspace_user" "estat" {
+  primary_email = "estat@andreag.com.br"
   password      = "34819d7beeabb9260a5c854bc85b3e44"
   hash_function = "MD5"
 
   name {
-    family_name = "estatisiticas"
+    family_name = "estatistica"
     given_name  = "Ninguem"
   }
 }
 
 resource "googleworkspace_group_member" "manager" {
-  group_id = googleworkspace_group.estatisiticas.id
-  email    = googleworkspace_estatisiticas.estatisiticas.primary_email
+  group_id = googleworkspace_group.estatisticas.id
+  email    = googleworkspace_user.estat.primary_email
 
   role = "MANAGER"
 }
